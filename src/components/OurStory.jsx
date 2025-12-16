f793481d45e289101967ba5c0255c8e4a87ba635
KaneloSefoloko <KaneloSefoloko@capitecbank.co.za>
2025-12-16 03:19:26 +0200

    production our page

--- src/components/OurStory.jsx
+++ src/components/OurStory.jsx
@@ -3,33 +3,39 @@ import React from 'react';
 const OurStory = () => (
     <div>
         {/* Hero Section */}
-        <div
-            className="relative h-[40vh] w-full bg-cover bg-center flex items-center justify-center"
-            style={{ backgroundImage: "url('/assets/ABOUT_US_4.webp')" }}
-        >
-            <div className="absolute inset-0 bg-gray-100/40" />
-        </div>
+        {/*<div*/}
+        {/*    className="relative h-[40vh] w-full bg-cover bg-center flex items-center justify-center"*/}
+        {/*    style={{ backgroundImage: "url('/assets/ABOUT_US_4.webp')" }}*/}
+        {/*>*/}
+        {/*    <div className="absolute inset-0 bg-gray-100/40" />*/}
+        {/*</div>*/}
 
         {/* Origin & Philosophy */}
-        <div className="max-w-4xl mx-auto py-12 px-4 space-y-6 text-center font-sans md:font-serif font-light">
-            <h1 className="text-3xl font-light text-black z-10">Our Story</h1>
-            <p>
-                Kavanti was born in the middle of the kasi noise founded in 2023 by Kanelo Sefoloko — where dreams feel
-                too big, far apart from the reality and chances feel too small.
-                The name stands for those who choose to rise anyway. Our designs are inspired by the streets we grew up
-                on, the music that raised us, and the courage that
-                it takes to stand out without asking for permission.
-                <br /><br />
-                Kavanti isn't just clothing. It's identity. It's for the ones who hustle with purpose. The ones who walk
-                with quiet confidence. The ones
-                who know they are becoming something greater day by day.
-                <br /><br />
-                Wear Kavanti. Don't just move through the world — leave a mark.
-            </p>
-            <p>
-                Our vision is brought to life through an innovative, 21st-century African design philosophy
-                that reflects the modern aspirations of South African youth.
-            </p>
+        <div className="pt-24">
+            <div
+                className="max-w-4xl mx-auto py-12 px-4 space-y-6 text-center font-sans md:font-serif font-light mb-36">
+                <h1 className="text-3xl font-light text-black z-10">Our Story</h1>
+                <p>
+                    Kavanti was born in the middle of the kasi noise founded in 2023 by Kanelo Sefoloko — where dreams
+                    feel
+                    too big, far apart from the reality and chances feel too small.
+                    The name stands for those who choose to rise anyway. Our designs are inspired by the streets we grew
+                    up
+                    on, the music that raised us, and the courage that
+                    it takes to stand out without asking for permission.
+                    <br/><br/>
+                    Kavanti isn't just clothing. It's identity. It's for the ones who hustle with purpose. The ones who
+                    walk
+                    with quiet confidence. The ones
+                    who know they are becoming something greater day by day.
+                    <br/><br/>
+                    Wear Kavanti. Don't just move through the world — leave a mark.
+                </p>
+                <p>
+                    Our vision is brought to life through an innovative, 21st-century African design philosophy
+                    that reflects the modern aspirations of South African youth.
+                </p>
+            </div>
         </div>
 
         {/* Responsive Image Section with Overlay */}
@@ -37,7 +43,6 @@ const OurStory = () => (
 
             <div
                 className="w-screen h-[200px] bg-no-repeat bg-center bg-contain"
-                style={{ backgroundImage: "url('/assets/mtho.svg')" }}
             >
 
             {/* Optional: Overlay for better readability */}
